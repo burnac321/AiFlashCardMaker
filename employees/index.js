be3cdee8b85a4534e91d78757145fb760fbc88a5
burnac321 <burnac321@gmail.com>
2026-07-02 12:01:23 +0200

    adding churchs contributions, amafaranga yirondo, insurance on employees table

--- employees/index.js
+++ employees/index.js
@@ -1,5 +1,8 @@
-const TAX_RATE = 0.23;
+const TAX_RATE = 0.20;
 const TAX_THRESHOLD = 70000;
+const CHURCH_RATE = 0.10;
+const SECURITY_RATE = 0.10;
+const INSURANCE_RATE = 0.10;
 
 const tbody = document.getElementById('tableBody');
 const summaryEl = document.getElementById('summary');
@@ -49,18 +52,43 @@ if (countInput === null) {
     } else {
         employees.forEach((emp, idx) => {
             let tax = 0;
-            if (emp.salary > TAX_THRESHOLD) {
-                tax = Math.round(emp.salary * TAX_RATE * 100) / 100;
-            }
-            totalTax += tax;
+            let healthInsurance = 0;
+            let securityContribution = 0;
+            let churchContribution = 0;
+            let salary = emp.salary;
 
-            const taxClass = tax > 0 ? 'tax-positive' : 'tax-zero';
+            function calculateChurchContribution(salary, CHURCH_RATE){
+                return salary * CHURCH_RATE
+            }
+
+            function calculateInsurance(salary, INSURANCE_RATE){
+                return salary * INSURANCE_RATE
+            }
+
+            function calculateSecurityContribution(salary, SECURITY_RATE){
+                return salary * SECURITY_RATE
+            }
+
+            function calculateTax(salary, TAX_RATE, TAX_THRESHOLD){
+                if(salary >= TAX_THRESHOLD){
+                return salary * TAX_RATE;
+                }
+            }
+            totalTax += calculateTax(salary, TAX_RATE, TAX_THRESHOLD);
+            healthInsurance = calculateInsurance(salary, INSURANCE_RATE);
+            churchContribution = calculateChurchContribution(salary, CHURCH_RATE);
+            securityContribution = calculateSecurityContribution(salary, SECURITY_RATE);
+            tax = calculateTax(salary, TAX_RATE, TAX_THRESHOLD);
+            totalTax += tax;
             const row = `<tr>
                 <td>${idx + 1}</td>
                 <td>${emp.name}</td>
                 <td>RWF${emp.salary.toFixed(2)}</td>
                 <td>${emp.job}</td>
-                <td class="${taxClass}">RWF ${tax.toFixed(2)}</td>
+                <td>RWF ${tax.toFixed(2)}</td>
+                <td> RWF ${churchContribution.toFixed(2)}</td>
+                <td>RWF ${healthInsurance.toFixed(2)}</td>
+                <td>RWF ${securityContribution.toFixed(2)}</td>
             </tr>`;
             htmlRows += row;
         });
